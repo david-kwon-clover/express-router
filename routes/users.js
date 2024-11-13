37bbe061b27145443414a2e478b1e08e925d15e7
David Kwon <david.kwon@clover.com>
2024-11-13 08:53:22 -0700

    Add PUT users/:id route

--- routes/users.js
+++ routes/users.js
@@ -3,36 +3,45 @@ const { User } = require("../models/index.js");
 const usersRouter = express.Router();
 
 usersRouter.get("/", async (req, res, next) => {
-    try {
-        const allUsers = await User.findAll();
-        if(!allUsers) {
-            throw new Error("No users to be found");
-        }
-        res.json(allUsers);
-    } catch (error) {
-        next(error);
+  try {
+    const allUsers = await User.findAll();
+    if (!allUsers) {
+      throw new Error("No users to be found");
     }
-})
+    res.json(allUsers);
+  } catch (error) {
+    next(error);
+  }
+});
 
 usersRouter.get("/:id", async (req, res, next) => {
-    try {
-        const targetUser = await User.findByPk(req.params.id);
-        if(!targetUser) {
-            throw new Error("No users to be found");
-        }
-        res.json(targetUser);
-    } catch (error) {
-        next(error);
+  try {
+    const targetUser = await User.findByPk(req.params.id);
+    if (!targetUser) {
+      throw new Error("No users to be found");
     }
-})
+    res.json(targetUser);
+  } catch (error) {
+    next(error);
+  }
+});
 
 usersRouter.post("/", async (req, res, next) => {
-    try {
-        const newUser = await User.create(req.body);
-        res.status(201).json(newUser);
-    } catch(error) {
-        next(error);
-    }
-})
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.put("/:id", async (req, res, next) => {
+  try {
+    const updatedUser = await User.update(req.body, { where: { id: req.params.id } });
+    res.status(201).json(updatedUser)
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = usersRouter;
